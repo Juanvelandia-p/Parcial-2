5d297c24c50a72ea66aa6845fb3322bb19eae27e
unknown <juan.velandia-p@labinfo.is.escuelaing.edu.co>
2026-03-24 17:11:31 -0500

    first commit

---
+++ src/main/java/co/escuelaing/edu/MathServiceApp.java
@@ -0,0 +1,14 @@
+
+package co.escuelaing.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathServiceApp.class, args);
+    }
+
+}
